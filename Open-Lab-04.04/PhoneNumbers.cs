3848d659f1ea52cc86cae3bf4b897746882ca291
MartinNovysedlak <martinnovysedlak48@gmail.com>
2019-11-14 09:45:00 +0100

    initial

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            var b = numbers;
+            return ($"({b[0]}{b[1]}{b[2]}) {b[3]}{b[4]}{b[5]}-{b[6]}{b[7]}{b[8]}{b[9]}");
         }
     }
 }
